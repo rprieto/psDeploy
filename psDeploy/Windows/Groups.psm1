ba7e359938d1b81c2045a9ec602e65b129d5fdb1
unknown <Administrator@.(none)>
2010-02-26 10:32:19 +1100

    Added Remove-UserFromGroup

--- psDeploy/Windows/Groups.psm1
+++ psDeploy/Windows/Groups.psm1
@@ -26,3 +26,35 @@ function Add-UserToGroup
         Write-Output "User '$Username' is already part of group '$Group'"
     }
 }
+
+
+<#
+.Synopsis
+Adds a user to a local user group
+#>
+function Remove-UserFromGroup
+{
+    param
+    (
+        [string] $Username = $(throw "Must provide the user name"),
+        [string] $Group = $(throw "Must provide the group name")
+    )
+    
+    $computer = $env:computername
+    $groupObj = [ADSI]"WinNT://$computer/$Group,group"
+    
+    # Check if the user is already part of the group
+    $existingUser = $groupObj.Members() | Where { [string] $_.GetType().InvokeMember("Name", 'GetProperty', $null, $_, $null) -eq $Username }
+     
+    if ($existingUser -ne $null)
+    {
+        $groupObj.Remove("WinNT://$Username")
+        Write-Output "Removed user '$Username' from group '$Group'"
+    }
+    else
+    {
+        Write-Output "User '$Username' is not part of group '$Group'"
+    }
+}
+
+
