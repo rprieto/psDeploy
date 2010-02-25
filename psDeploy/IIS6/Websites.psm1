eb12889ae6c17dc5d5a0d47ead831d84373881ee
unknown <Administrator@.(none)>
2010-02-26 08:53:31 +1100

    Stop website using WMI, added output messages

--- psDeploy/IIS6/Websites.psm1
+++ psDeploy/IIS6/Websites.psm1
@@ -11,22 +11,49 @@ function Start-WebSite
         [string] $Name = $(throw "Must provide a website name")
     )
 
-    $webServerSetting = Get-WmiObject -Namespace 'root\MicrosoftIISv2' -Class IISWebServerSetting -Filter "ServerComment = '$Name'" 
-    $webServers = Get-WmiObject -Namespace 'root\MicrosoftIISv2' -Class IIsWebServer
-    $targetServer = $webServers | Where-Object { $_.Name -eq $webServerSetting.Name }
-    $targetServer.Start()
+    $webServerSetting = Get-WmiObject -Namespace 'root\MicrosoftIISv2' -Class IISWebServerSetting -Filter "ServerComment = '$Name'"
+    
+    if ($webServerSetting)
+    {
+        $webServers = Get-WmiObject -Namespace 'root\MicrosoftIISv2' -Class IIsWebServer
+        $targetServer = $webServers | Where-Object { $_.Name -eq $webServerSetting.Name }
+        $targetServer.Start()
+        
+        Write-Output "Started website '$Name'"
+    }
+    else
+    {
+        Write-Output "Could not find website '$Name' to start"
+    }
 }
 
-#function StopWebsite([string]$siteName = $(throw "Must specify a site name"))
-#{
-#    $server = "servername"
-#    $siteName = "Default Web Site"
-#    $iis = [ADSI]"IIS://$server/W3SVC"
-#    $site = $iis.psbase.children | where { $_.keyType -eq "IIsWebServer" -AND
-#    $_.ServerComment -eq $siteName }
-#    $site.serverstate=4
-#    $site.setinfo()
-#}
+
+<#
+.Synopsis
+Starts the website
+#>
+function Stop-WebSite
+{
+    param
+    (
+        [string] $Name = $(throw "Must provide a website name")
+    )
+
+    $webServerSetting = Get-WmiObject -Namespace 'root\MicrosoftIISv2' -Class IISWebServerSetting -Filter "ServerComment = '$Name'"
+    
+    if ($webServerSetting)
+    {
+        $webServers = Get-WmiObject -Namespace 'root\MicrosoftIISv2' -Class IIsWebServer
+        $targetServer = $webServers | Where-Object { $_.Name -eq $webServerSetting.Name }
+        $targetServer.Stop()
+        
+        Write-Output "Stopped website '$Name'"
+    }
+    else
+    {
+        Write-Output "Could not find website '$Name' to stop"
+    }
+}
 
 
 <#
@@ -37,13 +64,19 @@ function Remove-WebSite
 {
     param
     (
-        [string]$Name = $(throw "Must provide a Site Name")
+        [string] $Name = $(throw "Must provide a Site Name")
     )
 
-	$webServerSetting = Get-WmiObject -Namespace 'root\MicrosoftIISv2' -Class IISWebServerSetting -Filter "ServerComment = '$Name'"
+	$webServerSetting = Get-WmiObject -Namespace "root\MicrosoftIISv2" -Class IISWebServerSetting -Filter "ServerComment = '$Name'"
+    
 	if ($webServerSetting)
     {
         $webServerSetting.Delete()
+        Write-Output "Deleted website '$Name'"
+    }
+    else
+    {
+        Write-Output "Could not find website '$Name' to delete"
     }
 }
 
@@ -77,7 +110,7 @@ function New-WebSite
 	$webSite = $service.CreateNewSite($Name, $bindings, $Path)
 
 	$webServerSetting = Get-WmiObject -Namespace 'root\MicrosoftIISv2' -Class IISWebServerSetting -Filter "ServerComment = '$Name'"
-	# This sets the applicationDefaults for the site 
+
 	$webServerSetting.AppPoolId = $AppPool
 	$webServerSetting.ServerAutoStart = $true
 	
@@ -102,41 +135,7 @@ function New-WebSite
 	$virtualDirSetting.AppPoolId = $AppPool
 	[Void]$virtualDirSetting.Put()
 	
-	Write-Debug ('Created {0}' -f $webServerSetting.Name)
-}
-
-
-
-<#
-.Synopsis
-Enables SSL for the website
-#>
-function Enable-SiteSelfSSL
-{
-    param
-    (
-        [string] $siteName = $(throw "Must provide a Site Name"),
-	    [string] $hostName = $(throw "Must provide a Host Name")
-	)
-	
-	$scriptFolder = & { (Split-Path $MyInvocation.ScriptName -Parent) }
-	$projectFolder = (Get-Item $scriptFolder).Parent.Parent.FullName
-	$selfSslExe = (Join-Path $projectFolder 'tools\SelfSSL\selfssl.exe')
-	$webSite = Get-WmiObject -Namespace "root/WebAdministration" -Class Site -Filter "Name = '$siteName'"
-	
-    if ($webSite)
-    {		
-		$startInfo = New-Object Diagnostics.ProcessStartInfo($selfSslExe)
-		$startInfo.UseShellExecute = $false
-		($startInfo.Arguments = ('/T /N:cn={0} /V:1000 /S:{1} /Q' -f $hostName,$webSite.Id))
-		$process = [Diagnostics.Process]::Start($startInfo)
-		$process.WaitForExit()
-		$exitCode = $process.ExitCode
-	}
-    else
-    {
-        throw "$siteName not found"
-    }
+	Write-Output "Created new website '$Name' pointing to '$Path'"
 }
 
 
