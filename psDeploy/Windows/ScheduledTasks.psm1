a4c8b7b700f55fe81e0113b5762eb64aee424d6c
rprieto <romain.prieto.web@gmail.com>
2010-03-01 16:06:45 +1100

    New scheduled tasks cmdlets (work in progress)

--- psDeploy/Windows/ScheduledTasks.psm1
+++ psDeploy/Windows/ScheduledTasks.psm1
@@ -1,59 +1,88 @@
 
 <#
 .Synopsis
+Gets info about an existing scheduled task
+#>
+function Get-ScheduledTask
+{
+    param
+    (
+        [string] $Name = $(throw "Must provide a task name")
+    )
+	
+    $tempCsv = [System.IO.Path]::GetTempFileName()
+    
+    Invoke-Expression "schtasks.exe /query /v /fo csv > $tempCsv"
+    $found = Import-Csv -Path $tempCsv | Where { $_.TaskName -eq $Name }
+    
+    Remove-Item $tempCsv -Force
+    
+    return $found
+}
+
+
+<#
+.Synopsis
+Enables an existing scheduled task
+#>
+function Enable-ScheduledTask
+{
+    param
+    (
+        [string] $Name = $(throw "Must provide a task name")
+    )
+}
+
+
+<#
+.Synopsis
+Disables an existing scheduled task
+#>
+function Disable-ScheduledTask
+{
+    param
+    (
+        [string] $Name = $(throw "Must provide a task name")
+    )
+}
+
+
+<#
+.Synopsis
 Creates a new scheduled task
 #>
 function New-ScheduledTask
 {
     param
     (
-        [string] $Path = $(throw 'Must provide a username'),
-        [string] $Name = $(throw 'Must provide a username')
-    ) 
-}
-
-
-function Get-ScheduledTasks
-{
-
-    param([string]$ComputerName = "localhost")
+    	[string]$Name = $(throw "Must provide a task name"),
+    	[string]$Path = $(throw "Must provide the path to the executable"),
+    	[string]$RunAs = "System",
+        
+    	[string]$Schedule = "Monthly",
+    	[string]$Modifier = "second",
+    	[string]$Days = "SUN",
+    	[string]$Months = '"MAR,JUN,SEP,DEC"',
+    	[string]$StartTime = "13:00",
+    	[string]$EndTime = "17:00",
+    	[string]$Interval = "60"	
+	)
     
 	Write-Host "Computer: $ComputerName"
-	$Command = "schtasks.exe /query"
+	$Command = "schtasks.exe /create /s $ComputerName /ru $RunAsUser /tn $TaskName /tr $TaskRun /sc $Schedule /mo $Modifier /d $Days /m $Months /st $StartTime /et $EndTime /ri $Interval /F"
 	Invoke-Expression $Command
-    
-    $tasks = Import-Csv -Path "c:\temp.csv"
-    $list = $tasks.GetType() | Select -Property "TaskName"
-	
-    
-    # Or with WMI
-
-
-    $strComputer = "."
-    $colItems = get-wmiobject -class "Win32_ScheduledJob" -namespace "root\CIMV2" -computername "localhost"
-
-    foreach ($objItem in $colItems)
-    {
-        write-host "Caption: " $objItem.Caption
-        write-host "Command: " $objItem.Command
-        write-host "Days Of Month: " $objItem.DaysOfMonth
-        write-host "Days Of Week: " $objItem.DaysOfWeek
-        write-host "Description: " $objItem.Description
-        write-host "Elapsed Time: " $objItem.ElapsedTime
-        write-host "Installation Date: " $objItem.InstallDate
-        write-host "Interact With Desktop: " $objItem.InteractWithDesktop
-        write-host "Job ID: " $objItem.JobId
-        write-host "Job Status: " $objItem.JobStatus
-        write-host "Name: " $objItem.Name
-        write-host "Notify: " $objItem.Notify
-        write-host "Owner: " $objItem.Owner
-        write-host "Priority: " $objItem.Priority
-        write-host "Run Repeatedly: " $objItem.RunRepeatedly
-        write-host "Start Time: " $objItem.StartTime
-        write-host "Status: " $objItem.Status
-        write-host "Time Submitted: " $objItem.TimeSubmitted
-        write-host "Until Time: " $objItem.UntilTime
-        write-host
-    }
-
 }
+
+
+<#
+.Synopsis
+Deletes an existing scheduled task
+#>
+function Remove-ScheduledTask
+{
+    param
+    (
+        [string] $Name = $(throw "Must provide a task name")
+    )
+}
+
