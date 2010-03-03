7d4474690e3a7d84a5110f23c87d2d2a13140e75
rprieto <romain.prieto.web@gmail.com>
2010-03-04 09:04:51 +1100

    Added basic cmdlets for shared folders

--- psDeploy/Windows/Shares.psm1
+++ psDeploy/Windows/Shares.psm1
@@ -1,10 +1,48 @@
 <#
 .Synopsis
+Gets a list of all the shared folders on this computer
+#>
+function Get-SharedFolders
+{
+    param
+    (
+        [string] $MachineName = $(hostname)
+    )
+    
+    Get-WmiObject -Class Win32_Share -ComputerName $MachineName
+}
+
+
+<#
+.Synopsis
 Creates a new shared folder (the folder has to exist)
 #>
 function New-SharedFolder
 {
+    param
+    (
+        [string] $Path = $(throw "Please provide the folder path"),
+        [string] $Name = $(throw "Please provide the share name"),
+        [int] $Type = 0,
+        [int] $MaxConnectionsAllowed,
+        [string] $Description = "New share",
+        [string] $Password,
+        [Win32_SecurityDescriptor] $Access
+    )
+    
+    $objWMI = Get-WmiObject -Class Win32_Share
+    
+    $result = $objWMI.Create($Path, $Name, $Type)
+    if ($result -eq 0)
+    {
+        Write-Output "Shared folder '$Name' created, pointing to '$Path'"
+    }
+    else
+    {
+        throw [RuntimeException] "Failed creating shared folder '$Name', error code = $result"
+    }
 }
+
 
 <#
 .Synopsis
