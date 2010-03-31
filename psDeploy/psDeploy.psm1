fa8ce945e4a78f194651a0d7795ebd21be42234b
rprieto <romain.prieto.web@gmail.com>
2010-03-31 16:54:53 +1100

    Sub modules are now loaded automatically (instead of maintaining a list)

--- psDeploy/psDeploy.psm1
+++ psDeploy/psDeploy.psm1
@@ -5,17 +5,8 @@
 
 
 $currentDir = Split-Path $MyInvocation.MyCommand.Path
-
-Import-Module -Name "$currentDir\Windows\Groups.psm1" -Force
-Import-Module -Name "$currentDir\Windows\ScheduledTasks.psm1" -Force
-Import-Module -Name "$currentDir\Windows\Users.psm1" -Force
-Import-Module -Name "$currentDir\Windows\Shares.psm1" -Force
-Import-Module -Name "$currentDir\Windows\Permissions.psm1" -Force
-Import-Module -Name "$currentDir\Windows\Network.psm1" -Force
-Import-Module -Name "$currentDir\Windows\Services.psm1" -Force
-Import-Module -Name "$currentDir\IIS6\AppPools.psm1" -Force
-Import-Module -Name "$currentDir\IIS6\Websites.psm1" -Force
-Import-Module -Name "$currentDir\IIS6\VirtualDirectories.psm1" -Force
+$modules = Get-ChildItem -Path $currentDir -Recurse -Include "*.psm1" -Exclude "psDeploy.psm1"
+$modules | %{ Import-Module -Name $_ -Force }
 
 
 
