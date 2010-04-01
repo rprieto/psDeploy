1978eb68f3bc292f849a6b4f453b79a7e3040186
rprieto <romain.prieto.web@gmail.com>
2010-04-01 11:23:06 +1100

    New cmdlet to update an existing Windows Service (stop copy restart)

--- psDeploy/Windows/Services.psm1
+++ psDeploy/Windows/Services.psm1
@@ -50,3 +50,43 @@ function Set-ServiceCredentials
 		Write-Output "Could not find service '$Name' for which to change credentials"
 	}
 }
+
+
+<#
+.Synopsis
+Deletes an existing service. Careful, there is no confirmation before deleting.
+#>
+function Update-Service
+{
+    param
+    (
+        [string] $Name = $(throw 'Must provide a service name'),
+        [string] $NewVersionPath = $(throw 'Must provide the path to the new service version')   
+    ) 
+    
+	$service = gwmi win32_service -filter "name='$Name'"
+	
+	if ($service -ne $null)
+	{
+        $service.StopService()
+        
+        
+        $isNewVersionSingleFile = Test-Path -Path $NewVersionPath -PathType Leaf
+        if ($isNewVersionSingleFile)
+        {
+            Copy-Item -Path $NewVersionPath -Destination $service.PathName -Force
+        }
+        else
+        {
+            $serviceFolder = Split-Path $service.PathName
+            Copy-Item -Path $UpdatePath -Destination $serviceFolder -Force
+        }
+        
+        $service.StartService()
+        Write-Host "Updated service '$Name' with new version from '$NewVersionPath'"
+	}
+	else
+	{
+		Write-Output "Could not find service '$Name' to update"
+	}
+}
