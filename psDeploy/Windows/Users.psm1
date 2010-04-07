8f2b33744be9cb5ba3b90a77b484a8dcea1853b6
rprieto <romain.prieto.web@gmail.com>
2010-04-07 15:03:33 +1000

    Added commandlet Remove-LocalUser

--- psDeploy/Windows/Users.psm1
+++ psDeploy/Windows/Users.psm1
@@ -6,22 +6,22 @@ function New-LocalUser
 {
     param
     (
-        [string] $Name = $(throw 'Muse provide a username'),
-        [string] $Password = $(throw 'Muse provide a password')
+        [string] $Name = $(throw 'Must provide a username'),
+        [string] $Password = $(throw 'Must provide a password')
     ) 
     
     $hostname = hostname   
     $computer = [adsi] "WinNT://$hostname"  
     
     $existingUser = $computer.psbase.children | Where { $_.Name -eq $Name }
-    
+      
     if ($existingUser -eq $null)
     {
         $userObj = $computer.Create("User", $Name)   
     
         $userObj.Put("description", "$Name")
         $userObj.SetInfo()
-        $userObj.SetPassword($Password)
+		$userObj.SetPassword($Password)
         $userObj.SetInfo()
         $userObj.psbase.invokeset("AccountDisabled", "False")
         $userObj.SetInfo()
@@ -31,5 +31,32 @@ function New-LocalUser
     else
     {
         Write-Output "User '$Name' already exists"
+    }
+}
+
+<#
+.Synopsis
+Deletes a local user
+#>
+function Remove-LocalUser
+{
+    param
+    (
+        [string] $Name = $(throw 'Must provide a username')
+    ) 
+
+    $hostname = hostname   
+    $computer = [adsi] "WinNT://$hostname"  
+    
+    $existingUser = $computer.psbase.children | Where { $_.Name -eq $Name }
+    
+    if ($existingUser -eq $null)
+    {
+        Write-Output "Could not find user '$Name' to delete"
+    }
+    else
+    {
+        $computer.psbase.children.remove($existingUser)
+        Write-Output "Deleted user '$Name'"
     }
 }
