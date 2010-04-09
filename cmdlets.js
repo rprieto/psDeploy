e88be66e78b99d92edfc806db7df38f75ebc2b4c
rprieto <romain.prieto.web@gmail.com>
2010-04-09 15:39:06 +1000

    Fixed code highlighting on page load

--- cmdlets.js
+++ cmdlets.js
@@ -1,8 +1,8 @@
 
-// Add code syntax highlighting
-prettyPrint();
-
 $(document).ready(function() {
+
+	// Add code syntax highlighting
+	prettyPrint();
 
 	// Make headers clickable to reveal content
 	$('.collapsible-header').click(function(){
