8231fdc805f8abe7c256b2e4b1cf1d0d6761b77b
rprieto <romain.prieto.web@gmail.com>
2010-04-09 21:14:05 +1000

    Added jQueryUI to have collapsible accordions, using them on cmdlet pages

--- cmdlets.js
+++ cmdlets.js
@@ -9,5 +9,19 @@ $(document).ready(function() {
 		$(this).next().toggle();
 		return false;
 	});
+
+	$(".accordion").accordion({
+		header: 'h4',
+		collapsible: true,
+		active: false,
+		autoHeight: false
+	});
+	
+	$('.collapsible-header').click(function(){
+		var x = $(this).parent().children(".collapsible-content");
+		$(this).parent().children(".collapsible-content").css('display', 'none');
+		$(this).next(".collapsible-content").css('display', 'block');
+		return false;
+	});
 	
 });
