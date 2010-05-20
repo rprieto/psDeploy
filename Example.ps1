081cd80a7508871dc7215a52d8b274ae9acb89f6
rprieto <romain.prieto.web@gmail.com>
2010-05-21 09:28:11 +1000

    Added real examples in the sample file

--- Example.ps1
+++ Example.ps1
@@ -10,6 +10,13 @@ try
     
     #
     # Insert deployment steps here
+    
+    Stop-IIS6AppPool -Name "MyPool"
+    New-LocalUser -Name "Bob" -Password "123456"
+    Expand-Zip -File "C:\Builds\Version3.zip" -Destination "C:\MyService" -CleanDestinationFirst
+    New-ScheduledTask -Name "MyTask" -Path "C:\Task.exe" -Every 2 -Weeks -On "Mon,Wed" -At 18:30
+
+    #
     #
 
     Write-DeploymentSuccess 'My app name'
