a811043e4e6956ff98a0c0a7796ec103dd3ba85e
rprieto <romain.prieto.web@gmail.com>
2010-05-20 21:50:40 +1000

    Updated example to use try/catch to handle failures

--- Example.ps1
+++ Example.ps1
@@ -1,20 +1,22 @@
-# Standard guards 
-Set-StrictMode -Version 2.0 
+Set-StrictMode -Version 2
 $ErrorActionPreference = 'Stop' 
-Trap [Exception] 
-{ 
-    Write-DeploymentFailure 'My app name' 
-    break 
+
+try
+{
+    # Load modules 
+    Set-Location (Split-Path $MyInvocation.MyCommand.Path) 
+    Import-Module .\psDeploy\psDeploy.psm1
+
+    # The actual install 
+    Start-Log -Name 'MyApp_' -AppendDate 
+    
+    #Stop-Service -DisplayName "Event log"    
+
+    # Finished!
+    Write-DeploymentSuccess 'My app name'
 }
-
-# Load modules 
-Set-Location (Split-Path $MyInvocation.MyCommand.Path) 
-Import-Module .\psDeploy\psDeploy.psm1
-
-# The actual install 
-Start-Log -Name 'MyApp_' -AppendDate 
-New-IIS6AppPool -Name 'Temp'
-Remove-IIS6AppPool -Name 'Temp'
-
-# Finished!
-Write-DeploymentSuccess 'My app name'
+catch
+{
+    $_ | Write-Warning
+    Write-DeploymentFailure 'My app name' 
+}
