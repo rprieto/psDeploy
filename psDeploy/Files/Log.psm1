fcf553f3f7548ec55443ed3c584a3a4b266937a8
rprieto <romain.prieto.web@gmail.com>
2010-05-20 11:30:50 +1000

    Added more logging cmdlets to deployment success/failure

--- psDeploy/Files/Log.psm1
+++ psDeploy/Files/Log.psm1
@@ -35,3 +35,74 @@ function Start-Log
         }
     }
 }
+
+
+<# 
+.Synopsis 
+Writes a success message to the console and global log file 
+#> 
+function Write-DeploymentSuccess 
+{    
+    param 
+    ( 
+        [string] $Application = $(throw 'Must specify the application name'), 
+        [string] $LogFile = 'C:\Logs\Deployments.txt' 
+    )
+
+        $message = " 
+    ------------------------------- 
+    $Application 
+    $($myInvocation.ScriptName) 
+    $(Get-Date) 
+    Deployment successful 
+    -------------------------------"    
+    
+    Write-Host $message 
+    Add-ToFile -Path $LogFile -Value $message
+}
+
+
+<# 
+.Synopsis 
+Writes a failure message to the console and global log file 
+#> 
+function Write-DeploymentFailure 
+{ 
+    param 
+    ( 
+        [string] $Application = $(throw 'Must specify the application name'), 
+        [string] $LogFile = 'C:\Logs\Deployments.txt' 
+    )
+
+        $message = " 
+    ------------------------------- 
+    $Application 
+    $($myInvocation.ScriptName) 
+    $(Get-Date) 
+    Deployment failed 
+    -------------------------------"   
+      
+    Write-Warning $message 
+    Add-ToFile -Path $LogFile -Value $message
+}
+
+
+<#
+.Synopsis 
+Alternative to Out-File, to work around the bug that doesn't create the full folder path
+#> 
+function Add-ToFile
+{
+    param
+    (
+        [string] $Path = $(throw 'File path is required'),
+        [string] $Value = $(throw 'Value to write is required')
+    )
+
+    if (-not (Test-Path $Path))
+    {
+        New-Item -Path $Path -Type File -Force | Out-Null
+    }
+    
+    $Value | Out-File -FilePath $Path -Append -Force
+}
